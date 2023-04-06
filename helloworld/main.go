ac3cbcc41e24732d93d696e83ca5f80b133b328d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-04-06 14:42:21 +0700

    feat(go): command arguments with conditions

--- helloworld/main.go
+++ helloworld/main.go
@@ -7,5 +7,11 @@ import (
 
 func main() {
 	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("Usage: ./helloworld <arguments>")
+		os.Exit(1)
+	}
+	
 	fmt.Printf("hello world\nOS.arg: %v\nArguments: %v\n", args, args[1:])
 }
