d51315b192df93febba74e347a3c8741da32c76b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-04-23 08:29:41 +0700

    :bug: (modules) new modules

--- 18-ssl-package/cmd/keygen/main.go
+++ 18-ssl-package/cmd/keygen/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/wardviaene/golang-for-devops-course/ssh-demo"
+	// "github.com/wardviaene/golang-for-devops-course/ssh-demo"
+	"github.com/PunGrumpy/go-ssh-key/ssh"
 )
 
 func main() {
@@ -12,12 +13,8 @@ func main() {
 		privateKey, publicKey []byte
 		err                   error
 	)
-	if privateKey, publicKey, err = ssh.GenerateKeys(); err != nil {
+	if privateKey, publicKey, err = ssh.GenerateKey(); err != nil {
 		fmt.Printf("unable to generate key pair: %v", err)
-	}
-	if privateKey, publicKey, err = ssh.GenerateKeys(); err != nil {
-		fmt.Printf("Error: %s\n", err)
-		os.Exit(1)
 	}
 	if err = os.WriteFile("mykey.pem", privateKey, 0600); err != nil {
 		fmt.Printf("Error: %s\n", err)
