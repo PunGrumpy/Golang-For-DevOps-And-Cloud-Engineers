769f1f39fa2e349432429d7671865ef1b063f957
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-03 23:56:06 +0700

    feat(go): discovery page

--- 19-idp/oidc-demo/pkg/server/discovery.go
+++ 19-idp/oidc-demo/pkg/server/discovery.go
@@ -1,9 +1,28 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/PunGrumpy/Golang-For-DevOps-And-Cloud-Engineers/19-idp/oidc-demo/pkg/oidc"
 )
 
 func (s *server) discovery(w http.ResponseWriter, r *http.Request) {
-
+	discovery := oidc.Discovery{
+		Issuer:                            s.Config.Url,
+		AuthorizationEndpoint:             s.Config.Url + "/authorize",
+		TokenEndpoint:                     s.Config.Url + "/token",
+		UserinfoEndpoint:                  s.Config.Url + "/userinfo",
+		JwksURI:                           s.Config.Url + "/jwks.json",
+		ScopesSupported:                   []string{"openid"},
+		ResponseTypesSupported:            []string{"code"},
+		TokenEndpointAuthMethodsSupported: []string{"none"},
+	}
+	out, err := json.Marshal(discovery)
+	if err != nil {
+		returnError(w, fmt.Errorf("error marshalling discovery: %s", err.Error()))
+		return
+	}
+	w.Write(out)
 }
