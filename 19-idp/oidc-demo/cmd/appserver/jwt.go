90813bd7b6c425a42cd7c9c44b2d353d33ba559d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-18 21:47:33 +0700

    feat: the jwks endpoint

--- 19-idp/oidc-demo/cmd/appserver/jwt.go
+++ 19-idp/oidc-demo/cmd/appserver/jwt.go
@@ -1,8 +1,54 @@
 package main
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/PunGrumpy/Golang-For-DevOps-And-Cloud-Engineers/19-idp/oidc-demo/pkg/oidc"
+	"github.com/golang-jwt/jwt/v4"
+)
 
 // gets token from tokenUrl validating token with jwksUrl and returning token & claims
-func getTokenFromCode(tokenUrl, jwksUrl, redirectUri, clientID, clientSecret, code string) (*jwt.Token, *jwt.StandardClaims, error) {
-	return nil, nil, nil
+func getTokenFromCode(tokenUrl, jwksUrl, redirectUri, clientID, clientSecret, code string) (*jwt.Token, *jwt.RegisteredClaims, error) {
+	values := url.Values{}
+	values.Add("grant_type", "authorization_code")
+	values.Add("client_id", clientID)
+	values.Add("client_secret", clientSecret)
+	values.Add("redirect_uri", redirectUri)
+	values.Add("code", code)
+
+	res, err := http.PostForm(tokenUrl, values)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, nil, fmt.Errorf("Error getting token: %s", body)
+	}
+
+	var token oidc.Token
+
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error unmarshalling token: %s", err)
+	}
+
+	claims := &jwt.RegisteredClaims{}
+	parsedToken, err := jwt.ParseWithClaims(token.IDToken, claims, func(*jwt.Token) (interface{}, error) {
+		return nil, nil
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("Error parsing token: %s", err)
+	}
+
+	return parsedToken, claims, fmt.Errorf("Not implemented")
 }
--- 19-idp/oidc-demo/pkg/server/jwks.go
+++ 19-idp/oidc-demo/pkg/server/jwks.go
@@ -1,9 +1,41 @@
 package server
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/PunGrumpy/Golang-For-DevOps-And-Cloud-Engineers/19-idp/oidc-demo/pkg/oidc"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func (s *server) jwks(w http.ResponseWriter, r *http.Request) {
 
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(s.PrivateKey)
+	if err != nil {
+		returnError(w, fmt.Errorf("parse private key error: %s", err))
+		return
+	}
+
+	publicKey := privateKey.PublicKey
+
+	jwks := oidc.Jwks{
+		Keys: []oidc.JwksKey{
+			{
+				Kid: "0-0-0-1",
+				Alg: "RS256",
+				Kty: "RSA",
+				Use: "sig",
+				N:   base64.StdEncoding.EncodeToString(publicKey.N.Bytes()),
+				E:   "AQAB",
+			},
+		},
+	}
+	out, err := json.Marshal(jwks)
+	if err != nil {
+		returnError(w, fmt.Errorf("jwks marshal error: %s", err))
+		return
+	}
+	w.Write(out)
 }
